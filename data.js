1c7037ba777b67d1a09ac3f8f9445c4062f156d2
AqeelAkhter <97888681+AqeelAkhter@users.noreply.github.com>
2024-08-15 04:36:35 -0700

    first

--- data.js
+++ data.js
@@ -27,6 +27,49 @@
 //     if (existingUser) {
 //       return res.status(400).json({ message: "User already exists" });
 //     }
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/user");
+
+const app = express();
+const PORT = 4000;
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+});
+
+const datamodal = mongoose.model("data", userSchema);
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/get", (req, res) => {
+  datamodal.find().then((resu) => {
+    res.json(resu);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  });
+});
+
+app.post("/voice", (req, res) => {
+});
+
+const Start = async () => {
+  app.listen(PORT, () => {
+    console.log("App is running on port",);
+  });
+};
+
+Start();
+
+
+
+
+
 
 //     const newUser = new User({ username, password, timeSlot: new Date() });
 //     await newUser.save();  
@@ -150,41 +193,4 @@
 
 //first get data code
 
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/user");
-
-const app = express();
-const PORT = 4000;
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
-});
-
-const datamodal = mongoose.model("data", userSchema);
-
-app.use(express.json());
-app.use(cors());
-
-app.get("/get", (req, res) => {
-  datamodal.find().then((resu) => {
-    res.json(resu);
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).send("Internal Server Error");
-  });
-});
-
-app.post("/voice", (req, res) => {
-});
-
-const Start = async () => {
-  app.listen(PORT, () => {
-    console.log("App is running on port",);
-  });
-};
-
-Start();
